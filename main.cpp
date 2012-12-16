0badde9a6455983d3fd706e32d643dfc8d98403e
Kevin R. André <hyperquantum@gmail.com>
2012-12-16 20:40:16 +0100

    created simple first program (does nothing yet)

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/* 
+   Copyright (C) 2011-2012, Kevin Andre <hyperquantum@gmail.com>
+   
+   This file is part of PMP (Party Music Player).
+   
+   PMP is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+   
+   PMP is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along
+   with PMP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <QtCore>
+
+
+int main(int argc, char *argv[]) {
+	
+	QCoreApplication a(argc, argv);
+	
+	QCoreApplication::setApplicationName("Party Music Player");
+	QCoreApplication::setApplicationVersion("0.0.0.1");
+	QCoreApplication::setOrganizationName("Party Music Player");
+	QCoreApplication::setOrganizationDomain("hyperquantum.be");
+	
+	
+	
+	return 0;
+}
