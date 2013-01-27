e36f094e9ce9bb3fc1cf8a1ce12bc6f9f4c7963a
Kevin R. André <hyperquantum@gmail.com>
2013-01-27 18:17:43 +0100

    wrote a simple test program to determine size and MD5 and SHA1 checksums of a complete file

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@
 
 #include <QtCore>
 
+#include <QCryptographicHash>
+
 
 int main(int argc, char *argv[]) {
 	
@@ -30,7 +32,24 @@ int main(int argc, char *argv[]) {
 	QCoreApplication::setOrganizationName("Party Music Player");
 	QCoreApplication::setOrganizationDomain("hyperquantum.be");
 	
+	QTextStream out(stdout);
 	
+	QFile file("this_is_a_test.mp3");
+	
+	if (file.open(QIODevice::ReadOnly)) {
+		QByteArray fileContents = file.readAll();
+		
+		QCryptographicHash md5_hasher(QCryptographicHash::Md5);
+		md5_hasher.addData(fileContents);
+		
+		QCryptographicHash sha1_hasher(QCryptographicHash::Sha1);
+		sha1_hasher.addData(fileContents);
+		
+		out << "File size: " << fileContents.length() << "\n";
+		out << "MD5 Hash:  " << md5_hasher.result().toHex() << "\n";
+		out << "SHA1 Hash: " << sha1_hasher.result().toHex() << "\n";
+		
+	}
 	
 	return 0;
 }
