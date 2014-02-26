0196cd8c01573cfd0f27222a7aa0fd299cdb9a68
Kevin André <hyperquantum@gmail.com>
2014-02-26 19:08:24 +0100

    added a simple HashID class, not used yet

---
+++ hashid.cpp
@@ -0,0 +1,31 @@
+/* 
+   Copyright (C) 2011-2014, Kevin Andre <hyperquantum@gmail.com>
+   
+   This file is part of PMP (Party Music Player).
+   
+   PMP is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+   
+   PMP is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along
+   with PMP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "hashid.h"
+
+namespace PMP {
+	
+	HashID::HashID(unsigned int length, const QByteArray& sha1,
+      const QByteArray& md5)
+    : _length(length), _sha1(sha1), _md5(md5)
+   {
+      //
+	}
+    
+}
---
+++ hashid.h
@@ -0,0 +1,53 @@
+/* 
+   Copyright (C) 2011-2014, Kevin Andre <hyperquantum@gmail.com>
+   
+   This file is part of PMP (Party Music Player).
+   
+   PMP is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+   
+   PMP is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along
+   with PMP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PMP_HASHID_H
+#define PMP_HASHID_H
+
+#include <QByteArray>
+
+namespace PMP {
+	
+	class HashID {
+	public:
+		HashID(unsigned int length, const QByteArray& sha1,
+			const QByteArray& md5);
+		
+		unsigned int length() const { return _length; }
+		const QByteArray& SHA1() const { return _sha1; }
+		const QByteArray& MD5() const { return _md5; }
+		
+	private:
+		unsigned int _length;
+		QByteArray _sha1;
+		QByteArray _md5;
+	};
+	
+	inline bool operator==(const HashID& me, const HashID& other) {
+		return me.length() == other.length()
+			&& me.SHA1() == other.SHA1()
+			&& me.MD5() == other.MD5();
+	}
+	
+	inline bool operator!=(const HashID& me, const HashID& other) {
+		return !(me == other);
+	}
+	
+}
+#endif
