ad9079bd0fe4f0c9c611d3272cfeabc04c73e2f5
Kevin André <hyperquantum@gmail.com>
2014-11-11 22:47:43 +0100

    removed file 'fixexecperm.sh' because it is longer needed, now that we use git instead of bzr

--- fixexecperm.sh
+++
@@ -1,7 +0,0 @@
-# This script is necessary when using bzr on Cygwin in combination with
-# an editor/IDE outside Cygwin.
-
-cd `dirname $0`
-
-chmod -x common/*.h common/*.cpp server/*.h server/*.cpp \
-  tools/*.cpp gui-remote/*.h gui-remote/*.cpp *.txt
