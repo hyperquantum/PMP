66440d5c2199238ee60084576f4fe417b104298b
Kevin André <hyperquantum@gmail.com>
2015-01-01 17:59:21 +0100

    updated copyright year(s) for 2015

--- tools/hash-main.cpp
+++ tools/hash-main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2011-2014, Kevin Andre <hyperquantum@gmail.com>
+    Copyright (C) 2011-2015, Kevin Andre <hyperquantum@gmail.com>
 
     This file is part of PMP (Party Music Player).
 
