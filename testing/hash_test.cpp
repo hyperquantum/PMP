c2aaab3456ad9e4a7da2756920fe680657ef3dc1
Kevin André <hyperquantum@gmail.com>
2017-04-19 21:37:35 +0200

    More extensive unit testing for the hash.
    In the unit test we are now doing several modifications (in a memory buffer) to the test file. We check if the modifications are real, we calculate the hash for each of them, and we check if the resulting hashes are all the same, the one we expect it to be.
    
    But more types of modifications are still needed. We need to try to change as many individual tag fields as possible, for all kinds of tag types (ID3v1, ID3v2, ...), and then see if all those still result in the hash value we expect.

--- testing/hash_test.cpp
+++ testing/hash_test.cpp
@@ -24,6 +24,9 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QTextStream>
+#include <QVector>
+
+#include <functional>
 
 /* TagLib includes */
 #include <flacfile.h>
@@ -34,6 +37,20 @@
 #include <tbytevectorstream.h>
 
 using namespace PMP;
+
+QString checksum(TagLib::ByteVector const& data) {
+    QCryptographicHash sha1Hasher(QCryptographicHash::Sha1);
+    sha1Hasher.addData(data.data(), data.size());
+
+    return sha1Hasher.result().toHex();
+}
+
+QString getHashAsString(const FileHash& hash) {
+    if (hash.empty()) return "empty";
+
+    return QString::number(hash.length())
+            + "-" + hash.MD5().toHex() + "-" + hash.SHA1().toHex();
+}
 
 TagLib::File* createFileObject(TagLib::ByteVectorStream& fileStream, QString extension) {
     QString lowercaseExtension = extension.toLower();
@@ -51,18 +68,173 @@ TagLib::File* createFileObject(TagLib::ByteVectorStream& fileStream, QString ext
     return nullptr;
 }
 
-QString checksum(TagLib::ByteVector const& data) {
-    QCryptographicHash sha1Hasher(QCryptographicHash::Sha1);
-    sha1Hasher.addData(data.data(), data.size());
+QList<std::function<void (TagLib::File*)> > getModifiers(QString extension) {
+    QList<std::function<void (TagLib::File*)> > modifiers;
 
-    return sha1Hasher.result().toHex();
+    modifiers.append([](TagLib::File* file) { file->tag()->setTitle("Ooooooooo"); });
+    modifiers.append([](TagLib::File* file) { file->tag()->setArtist("Aaaaaaaaaa"); });
+    modifiers.append([](TagLib::File* file) { file->tag()->setAlbum("Eeeeeeeeee"); });
+    modifiers.append([](TagLib::File* file) { file->tag()->setYear(2099); });
+    modifiers.append([](TagLib::File* file) { file->tag()->setComment("No comment!"); });
+    modifiers.append([](TagLib::File* file) {  });
+
+    // TODO: format-specific file modifications
+
+    return modifiers;
 }
 
-QString getHashAsString(const FileHash& hash) {
-    if (hash.empty()) return "empty";
+class FileTester {
+public:
+    FileTester(QString filename, QString expectedResult)
+     : _out(stdout), _err(stderr), _error(false), _filename(filename),
+       _expectedResult(expectedResult)
+    {
+        QFileInfo fileInfo(filename);
+        _extension = fileInfo.suffix();
 
-    return QString::number(hash.length())
-            + "-" + hash.MD5().toHex() + "-" + hash.SHA1().toHex();
+        if (!FileAnalyzer::isExtensionSupported(_extension, true)) {
+            _err << "File extension not supported: " << _extension << endl;
+            _error = true;
+            return;
+        }
+
+        QFile file(filename);
+        if (!file.open(QIODevice::ReadOnly)) {
+            _err << "Could not open file: " << filename << endl;
+            _error = true;
+            return;
+        }
+
+        FileAnalyzer analyzer(fileInfo);
+        analyzer.analyze();
+
+        if (!analyzer.analysisDone()) {
+            if (expectedResult == "invalid") return; /* OK */
+
+            _err << "File analysis FAILED unexpectedly for " << filename << endl;
+            _error = true;
+            return;
+        }
+
+        _originalResult = getHashAsString(analyzer.hash());
+
+        if (_originalResult != expectedResult) {
+            _err << "Hash MISMATCH!" << endl
+                 << "Filename: " << filename << endl
+                 << "Expected: " << expectedResult << endl
+                 << "Actual:   " << _originalResult << endl;
+            _error = true;
+            return;
+        }
+
+        QByteArray contents = file.readAll();
+        _originalFileContents.setData(contents.data(), contents.length());
+        _out << "Original data checksum: " << checksum(_originalFileContents) << endl;
+    }
+
+    bool error() const { return _error; }
+
+    const TagLib::ByteVector& originalFileContents() const {
+        return _originalFileContents;
+    }
+
+    const QString& extension() const { return _extension; }
+
+    const QString& originalResult() const { return _originalResult; }
+
+    bool testModifications(QList<std::function<void (TagLib::File*)> > modifiers) {
+        QVector<TagLib::ByteVector> transformed;
+        transformed.reserve(modifiers.size());
+
+        /* Apply all modifications, and make sure they really are different modifications
+           of the original. */
+        Q_FOREACH(auto modifier, modifiers) {
+            auto modifiedData = applyModification(modifier);
+            _out << "Modified data checksum: " << checksum(modifiedData) << endl;
+
+            if (modifiedData == originalFileContents()) {
+                _err << "Problem: modification ineffective; test would be unreliable"
+                     << endl;
+                return false;
+            }
+
+            if (transformed.contains(modifiedData)) {
+                _err << "Problem: modification not unique; test would be unreliable"
+                     << endl;
+                return false;
+            }
+
+            transformed.append(modifiedData);
+        }
+
+        Q_FOREACH(auto modifiedData, transformed) {
+            FileAnalyzer analyzer(modifiedData, extension());
+            analyzer.analyze();
+            if (!analyzer.analysisDone()) {
+                _err << "File analysis FAILED on modified data!" << endl;
+                return false;
+            }
+
+            QString modifiedHash = getHashAsString(analyzer.hash());
+            if (modifiedHash != _expectedResult) {
+                _err << "Hash MISMATCH after modification!" << endl
+                     << "Filename: " << _filename << endl
+                     << "Expected: " << _expectedResult << endl
+                     << "Original: " << originalResult() << endl
+                     << "Modified: " << modifiedHash << endl;
+                return false;
+            }
+
+            _out << "Modification resulted in correct hash." << endl;
+        }
+
+        return true;
+    }
+
+private:
+    TagLib::ByteVector applyModification(
+            std::function<TagLib::ByteVector (const TagLib::ByteVector&)> modifier)
+    {
+        return modifier(originalFileContents());
+    }
+
+    TagLib::ByteVector applyModification(std::function<void (TagLib::File*)> modifier) {
+        /* create a stream from the original file contents (makes a copy) */
+        TagLib::ByteVectorStream scratchStream(originalFileContents());
+
+        TagLib::File* scratchFile = createFileObject(scratchStream, extension());
+
+        if (scratchFile->readOnly() || !scratchFile->isOpen() || !scratchFile->isValid())
+        {
+            _err << "Problem when modifying tags: scratch file not modifyable" << endl;
+            return TagLib::ByteVector();
+        }
+
+        modifier(scratchFile);
+
+        if (!scratchFile->save()) {
+            _err << "Problem when saving modified scratch file to scratch stream" << endl;
+            return TagLib::ByteVector();
+        }
+
+        /* get the modified data from the scratch stream */
+        return *scratchStream.data();
+    }
+
+
+    QTextStream _out;
+    QTextStream _err;
+    bool _error;
+    QString _filename;
+    QString _extension;
+    QString _expectedResult;
+    TagLib::ByteVector _originalFileContents;
+    QString _originalResult;
+};
+
+void setArtistAndYear(TagLib::File* file) {
+    file->tag()->setArtist("Aaaaaaaaaa");
+    file->tag()->setYear(2099);
 }
 
 int main(int argc, char *argv[]) {
@@ -77,103 +249,24 @@ int main(int argc, char *argv[]) {
     QTextStream out(stdout);
     QTextStream err(stderr);
 
-    if (QCoreApplication::arguments().size() != 3) { /* exe-name + 2 args */
-        err << "Exactly two arguments are required." << endl;
+    /* usage: PMP-HashTool <filename> <expected hash> */
+    QStringList arguments = QCoreApplication::arguments();
+    if (arguments.size() != 3 || arguments[1].isEmpty() || arguments[2].isEmpty())
+    {
+        err << "Exactly two non-empty arguments are required." << endl;
         return 2;
     }
 
-    QString fileName = QCoreApplication::arguments()[1];
-    QString expectedResult = QCoreApplication::arguments()[2];
+    QString filename = arguments[1];
+    QString expectedResult = arguments[2];
 
-    QFileInfo fileInfo(fileName);
-    QString extension = fileInfo.suffix();
+    FileTester tester(filename, expectedResult);
+    if (tester.error()) return 1;
 
-    if (!FileAnalyzer::isExtensionSupported(extension, true)) {
-        err << "File extension not supported: " << extension << endl;
-        return 2;
-    }
+    bool result = tester.testModifications(getModifiers(tester.extension()));
+    if (!result) return 1;
 
-    QFile file(fileName);
-    if (!file.open(QIODevice::ReadOnly)) {
-        err << "Could not open file: " << fileName << endl;
-        return 2;
-    }
-
-    FileAnalyzer analyzer(fileInfo);
-    analyzer.analyze();
-
-    if (!analyzer.analysisDone()) {
-        if (expectedResult == "invalid") return 0; /* OK */
-
-        err << "File analysis FAILED unexpectedly." << endl;
-        return 1;
-    }
-
-    QString hashAsString = getHashAsString(analyzer.hash());
-
-    if (hashAsString != expectedResult) {
-        err << "Hash MISMATCH!" << endl
-            << "Filename: " << fileName << endl
-            << "Expected: " << expectedResult << endl
-            << "Actual:   " << hashAsString << endl;
-        return 1;
-    }
-
-    QByteArray contents = file.readAll();
-    TagLib::ByteVector contentsOriginal(contents.data(), contents.length());
-    QString originalChecksum = checksum(contentsOriginal);
-
-    TagLib::ByteVector scratch(contentsOriginal);
-    TagLib::ByteVectorStream scratchStream(scratch);
-    TagLib::File* scratchFile = createFileObject(scratchStream, extension);
-    if (scratchFile->readOnly() || !scratchFile->isOpen() || !scratchFile->isValid()) {
-        err << "Scratch memory not valid or readonly!" << endl
-            << "Filename: " << fileName << endl;
-        return 1;
-    }
-
-    if (scratchFile->tag()->year() != 2016) {
-        err << "Tags not read correctly!" << endl
-            << "Filename: " << fileName << endl;
-        return 1;
-    }
-
-    scratchFile->tag()->setArtist("Aaaaaaaaaa");
-    scratchFile->tag()->setYear(2099);
-    if (!scratchFile->save()) {
-        err << "Call to save() failed!" << endl
-            << "Filename: " << fileName << endl;
-        return 1;
-    }
-
-    /* Get the bytevector from the stream again, because the stream keeps its changes
-     * local. */
-    scratch = *scratchStream.data();
-
-    if (scratch == contentsOriginal) {
-        err << "Changing artist in scratch buffer did not have any effect!" << endl
-            << "Filename: " << fileName << endl
-            << "Checksum of original: " << originalChecksum << endl
-            << "Checksum of scratch:  " << checksum(scratch);
-        return 1;
-    }
-
-    FileAnalyzer analyzer2(scratch, extension);
-    analyzer2.analyze();
-    if (!analyzer2.analysisDone()) {
-        err << "File analysis FAILED during scratch operations!" << endl;
-        return 1;
-    }
-
-    QString scratchHash = getHashAsString(analyzer2.hash());
-    if (scratchHash != expectedResult) {
-        err << "Hash MISMATCH after scratch operation!" << endl
-            << "Filename: " << fileName << endl
-            << "Expected: " << expectedResult << endl
-            << "Original: " << hashAsString << endl
-            << "Modified: " << scratchHash << endl;
-        return 1;
-    }
+    out << "Success!" << endl;
 
     return 0;
 }
