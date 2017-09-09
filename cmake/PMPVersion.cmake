9c7151e73515c734e595dd8ddc5cdfe0dd235633
Kevin André <hyperquantum@gmail.com>
2017-09-09 21:28:10 +0200

    Move PMP version info out of "version.h".
    Make version.h an auto-generated file that is generated from version.h.in, using info that is now stored in "PMPVersion.cmake".

---
+++ cmake/PMPVersion.cmake
@@ -0,0 +1,17 @@
+
+# set version and branding info
+set(PMP_VERSION_MAJOR 0)
+set(PMP_VERSION_MINOR 0)
+set(PMP_VERSION_PATCH 3)
+set(PMP_ORGANIZATION_NAME "Party Music Player")
+set(PMP_ORGANIZATION_DOMAIN "hyperquantum.be")
+set(PMP_WEBSITE "http://hyperquantum.be/pmp")
+set(PMP_BUGREPORT_LOCATION "https://github.com/hyperquantum/PMP/issues")
+
+# create derived info
+
+set(
+    PMP_VERSION_MAJORMINORPATCH
+    ${PMP_VERSION_MAJOR}.${PMP_VERSION_MINOR}.${PMP_VERSION_PATCH}
+)
+set(PMP_VERSION_DISPLAY "${PMP_VERSION_MAJORMINORPATCH}")
