f77146d973b9df02e5c6dc07db02df4cad0e1980
Kevin André <hyperquantum@gmail.com>
2021-09-18 17:27:13 +0200

    Change braces style

--- src/gui-remote/gui-remote-main.cpp
+++ src/gui-remote/gui-remote-main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014-2020, Kevin Andre <hyperquantum@gmail.com>
+    Copyright (C) 2014-2021, Kevin Andre <hyperquantum@gmail.com>
 
     This file is part of PMP (Party Music Player).
 
@@ -27,8 +27,8 @@
 
 using namespace PMP;
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
 
     QCoreApplication::setApplicationName("Party Music Player - Remote");
