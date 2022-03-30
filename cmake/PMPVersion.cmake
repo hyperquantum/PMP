980edd7242a0f585ecc7e89c20c75f76bc23b167
Kevin André <hyperquantum@gmail.com>
2022-03-30 15:08:42 +0200

    Put version and branding separately

--- cmake/PMPVersion.cmake
+++ cmake/PMPVersion.cmake
@@ -1,15 +1,17 @@
 
-# set version and branding info
+# define version information
 set(PMP_VERSION_MAJOR 0)
 set(PMP_VERSION_MINOR 0)
 set(PMP_VERSION_PATCH 7)
+
+# define branding information
 set(PMP_ORGANIZATION_NAME "Party Music Player")
 set(PMP_ORGANIZATION_DOMAIN "hyperquantum.be")
 set(PMP_WEBSITE "http://hyperquantum.be/pmp")
 set(PMP_BUGREPORT_LOCATION "https://github.com/hyperquantum/PMP/issues")
 set(PMP_COPYRIGHT_YEARS "2014-2022")
 
-# create derived info
+# create derived variables
 
 set(
     PMP_VERSION_MAJORMINORPATCH
