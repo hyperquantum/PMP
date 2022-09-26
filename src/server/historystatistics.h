85fe162cbf3b703278da1484e2509c567c79ad52
Kevin André <hyperquantum@gmail.com>
2022-09-26 20:15:56 +0200

    Delete the old HistoryStatistics class

--- src/server/historystatistics.h
+++ src/server/historystatistics.h
@@ -23,7 +23,6 @@
 #include "common/future.h"
 #include "common/resultorerror.h"
 
-#include "databaserecords.h"
 #include "trackstats.h"
 
 #include <QDateTime>
@@ -81,102 +80,5 @@ namespace PMP
         QMutex _mutex;
         QHash<quint32, UserStatisticsEntry> _userData;
     };
-
-    /*
-    class HistoryStatistics : public QObject
-    {
-        Q_OBJECT
-    public:
-        HistoryStatistics(QObject* parent, HashRelations* hashRelations);
-
-        /// Add an entry to the history, update statistics, and return the IDs of the
-        /// hashes whose statistics have changed.
-        ResultOrError<SuccessType, FailureType> addToHistory(quint32 userId,
-                                                             quint32 hashId,
-                                                             QDateTime start,
-                                                             QDateTime end,
-                                                             int permillage,
-                                                             bool validForScoring);
-
-        bool wasFetchedAlready(quint32 userId, uint hashId);
-        Nullable<TrackStats> tryGet(quint32 userId, uint hashId);
-
-        ResultOrError<TrackStats, FailureType> getOrFetch(quint32 userId, uint hashId);
-
-    Q_SIGNALS:
-        void hashStatisticsChanged(quint32 userId, QVector<uint> hashIds);
-
-    private:
-        struct UserHashStatisticsEntry
-        {
-            UserHashStatisticsEntry() : userId(0), hashId(0) {}
-
-            bool updateIndividualStatsWithNewerOnes(TrackStats const& maybeNewerStats)
-            {
-                if (individualStats.isNull())
-                {
-                    individualStats = maybeNewerStats;
-                    return true;
-                }
-
-                return individualStats.value().updateWithNewerStats(maybeNewerStats);
-            }
-
-            bool updateMissingIndividualStatsToNeverPlayed()
-            {
-                if (!individualStats.isNull())
-                    return false;
-
-                individualStats = TrackStats{};
-                return true;
-            }
-
-            void setIndividualStatsAsGroupStats()
-            {
-                groupStats = individualStats;
-            }
-
-            void setGroupStats(TrackStats& stats)
-            {
-                groupStats = stats;
-            }
-
-            quint32 userId;
-            uint hashId;
-            Nullable<TrackStats> individualStats;
-            Nullable<TrackStats> groupStats;
-        };
-
-        struct UserStatisticsEntry
-        {
-            QHash<uint, UserHashStatisticsEntry> hashData;
-            //QSet<uint> hashesToFetch;
-        };
-
-        /// Fetch statistics for a specific user and hash. Will also fetch statistics
-        /// for related hashes if missing. Returns a list of hashes whose statistics have
-        /// changed.
-        ResultOrError<QVector<uint>, FailureType> fetch(quint32 userId, uint hashId);
-
-        /// From a list of specified hashes, return the subset of hashes whose statistics
-        /// are still missing.
-        QVector<uint> getHashesWithMissingStats(quint32 userId, QSet<uint> hashIds);
-
-        /// Apply the hash stats that have been fetched from the database, and return a
-        /// list of the hashes whose data have been changed as a result.
-        QVector<uint> applyFetchedStats(quint32 userId,
-                                        QVector<uint> const& hashesToFetch,
-                           QVector<DatabaseRecords::HashHistoryStats> const& fetchResult);
-
-        void recalculateGroupStats(quint32 userId, QVector<uint> hashIds);
-        ResultOrError<SuccessType, FailureType> fetchMissingRelatedStats(
-                                                       UserHashStatisticsEntry& hashData);
-        UserHashStatisticsEntry& getOrCreateHashEntry(quint32 userId, uint hashId);
-
-        HashRelations* _hashRelations;
-        QMutex _mutex;
-        QHash<quint32, UserStatisticsEntry> _userData;
-    };
-    */
 }
 #endif
