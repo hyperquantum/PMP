69a4b3c842cee34967580e41e05d393bec575e04
Kevin André <hyperquantum@gmail.com>
2022-11-23 17:40:38 +0100

    Change braces style

--- src/common/collectionfetcher.h
+++ src/common/collectionfetcher.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2020, Kevin Andre <hyperquantum@gmail.com>
+    Copyright (C) 2020-2022, Kevin Andre <hyperquantum@gmail.com>
 
     This file is part of PMP (Party Music Player).
 
@@ -25,9 +25,10 @@
 #include <QObject>
 #include <QVector>
 
-namespace PMP {
-
-    class CollectionFetcher : public QObject {
+namespace PMP
+{
+    class CollectionFetcher : public QObject
+    {
         Q_OBJECT
     public:
         CollectionFetcher() {}
