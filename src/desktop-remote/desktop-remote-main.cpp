7608bae02da98680ecfc8836033da1a79f46b32f
Kevin André <hyperquantum@gmail.com>
2024-10-15 21:45:56 +0200

    Remove obsolete attributes about high DPI
    Those attributes no longer have any effect in Qt 6.

--- src/desktop-remote/desktop-remote-main.cpp
+++ src/desktop-remote/desktop-remote-main.cpp
@@ -29,9 +29,6 @@ using namespace PMP;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-
     QApplication app(argc, argv);
 
     QCoreApplication::setApplicationName("Party Music Player - Remote");
